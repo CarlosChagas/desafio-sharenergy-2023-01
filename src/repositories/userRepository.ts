6f1b86c632f293c17bf1fff6ee3018e13d04f20d
Carlos Chagas <carloschagasdev@gmail.com>
2023-01-08 20:54:31 -0300

    Implementação das funcões de autenticação, verificação de credenciais (usuário e senha) de login, em repositório local(mock)

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,6 +1,16 @@
-
-async function authenticateUser(user: Iuser){
-    return user
+const userDb: Array<Iuser> = [
+    {
+        username: 'desafiosharenergy',
+        password: '$2a$10$YRkDZ.GPfYjyYY5Ccw44bOJN7rwBsFtiT/sKS8emzJs6rw/uJHfmi'
+    },
+    {
+        username: 'desafiosharenergy2',
+        password: '$2a$10$zkGHdc6TRXLjAZl7xj096e/JB6G.KZx0614l/OzVCpnA9GjeAHeBa'
+    }
+]
+async function getUserByName(getUsername: string){
+    let getUser = userDb.find(({ username }) => getUsername === username)
+    return getUser
 }
 
-export default {authenticateUser}
+export default {getUserByName}
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,10 +1,34 @@
 import userRepository from '../repositories/userRepository'
+import { compare } from 'bcrypt-ts'
 
-async function authenticateUser(user: Iuser) {
-        
-        return await userRepository.authenticateUser(user)
-    
+
+async function authenticateUser(userLogin: Iuser) {
+
+    if (await checkLoginCredentials(userLogin)) {
+        return await userRepository.getUserByName(userLogin.username)
+    } else {
+        throw new Error('Check login credentials')
+    }
 }
 
-export default {authenticateUser}
+async function checkLoginCredentials(userLogin: Iuser) {
+    let getUser = await userRepository.getUserByName(userLogin.username)
+
+    if (getUser == undefined) {
+        return false
+    } else {
+        if (await validPassword(userLogin.password, getUser.password)) {
+            return true
+        }
+    }
+}
+
+async function validPassword(password: string, encryptedPassword: string) {
+    if (!await compare(password, encryptedPassword)) {
+        throw new Error('Invalid credentials!')
+    }
+    return true
+}
+
+export default { authenticateUser }
 
