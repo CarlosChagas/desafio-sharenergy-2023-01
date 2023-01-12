4a7d482253c7cf3da02b45b667be991ccdb60156
Carlos Chagas <carloschagasdev@gmail.com>
2023-01-12 10:43:43 -0300

    Correção de imports

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,10 @@
-import Fastify, { FastifyInstance } from "fastify"
+import { FastifyInstance } from "fastify"
 import userController from "../controllers/userController"
-import userRepository from "../repositories/userRepository"
-import decrypt from "../security/jwt"
-
 
 async function routes(fastify: FastifyInstance) {
 
     fastify.post('/auth', userController.authenticate)
-    
+
 
 }
 export default routes
